0a6b6ca6fd413bacfe99cd08cac96d6af788e759
Max Meier <max.meier.089@googlemail.com>
2020-10-30 15:47:04 +0100

    TaskCreation Update

--- TaskExamples/TaskCreation.cs
+++ TaskExamples/TaskCreation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskExamples
@@ -34,16 +35,113 @@ namespace TaskExamples
             task.Start();
 
 
+            // method with return type
+            Task<int> intTask = Task.Run(MyIntMethod);
+            int result = intTask.Result;
+            Console.WriteLine("Result: " + result);
+
+
+            // anonymous method with return type
+            result = Task.Run(() => { return 99; }).Result;
+            Console.WriteLine("Result: " + result);
+
+
+            // task wait
+            Task t0 = Task.Run(() => { Console.WriteLine("T1"); });
+            Task t1 = Task.Delay(1000);
+            Task t2 = Task.Delay(500);
+
+            Task[] tasks = new Task[] { t0, t1, t2 };
+
+            // wait for any task to complete
+            int index = Task.WaitAny(tasks);
+            Console.WriteLine("First task to complete: " + index);
+
+            // wait for all tasks to complete
+            Task.WaitAll(tasks);
+            Console.WriteLine("Now all tasks have completed!");
+
+
+            // task cancellation with a boolean variable
+            Console.WriteLine("Task cancellation (manual)");
+
+            bool tasksRunning = true;
+
+            void doSomething()
+            {
+                Random r = new Random();
+
+                while (tasksRunning)
+                {
+                    Console.WriteLine("work...");
+
+                    // wait 1 second
+                    Task.Delay(1000).Wait();
+
+                    if (r.Next(10) == 0)
+                    {
+                        Console.WriteLine("Cancel!");
+                        tasksRunning = false;
+                    }
+                }
+            }
+
+            Task.WaitAll(new Task[] { Task.Run(doSomething), Task.Run(doSomething), Task.Run(doSomething) });
+
+
+            // task cancellation with a CancellationToken
+            Console.WriteLine("Task cancellation (CancellationToken)");
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            cancellationToken.Register(() => { Console.WriteLine("Cancel!"); });
+
+            void doSomethingElse()
+            {
+                Random r = new Random();
+
+                while (true)
+                {
+                    Console.WriteLine("work...");
+
+                    // wait 1 second
+                    Task.Delay(1000).Wait();
+
+                    if (r.Next(10) == 0)
+                    {
+                        cancellationTokenSource.Cancel();
+                    }
+
+                    try
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                    }
+                    catch (Exception ex)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Task.WaitAll(new Task[] { Task.Run(doSomethingElse), Task.Run(doSomethingElse), Task.Run(doSomethingElse) });
+
+
             // background worker
             BackgroundWorker worker = new BackgroundWorker();
             worker.DoWork += Worker_DoWork;
             worker.RunWorkerAsync();
         }
-       
+
 
         void MyMethod()
         {
             Console.WriteLine("MyMethod: " + (Task.CurrentId.HasValue ? Task.CurrentId.Value.ToString() : "-"));
+        }
+
+        int MyIntMethod()
+        {
+            Console.WriteLine("MyIntMethod: " + (Task.CurrentId.HasValue ? Task.CurrentId.Value.ToString() : "-"));
+            return 7;
         }
 
         static void MyStaticMethod()
